f329bbeb6355a252a66fdbd5f94da36928387f9a
obijuan <juan@iearobotics.com>
2019-04-25 17:56:04 +0200

    ftdi04: working version

--- wiki/2018-04-23-USB-node/ftdimin-04/ftdimin04.cc
+++ wiki/2018-04-23-USB-node/ftdimin-04/ftdimin04.cc
@@ -4,11 +4,26 @@
 
 namespace demo {
 
+using v8::FunctionCallbackInfo;
+using v8::HandleScope;
+using v8::Isolate;
 using v8::Local;
+using v8::NewStringType;
 using v8::Object;
+using v8::String;
+using v8::Value;
+
+void Method(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
+  //-- Establecer el valor de retorno: Cadena "Hola!"
+  args.GetReturnValue().Set(String::NewFromUtf8(
+      isolate, "Hola!", NewStringType::kNormal).ToLocalChecked());
+}
 
 void InitAll(Local<Object> exports) {
   FtdiContextWrapper::Init(exports);
+  NODE_SET_METHOD(exports, "hello", Method);
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, InitAll)
--- wiki/2018-04-23-USB-node/ftdimin-04/test.js
+++ wiki/2018-04-23-USB-node/ftdimin-04/test.js
@@ -1,5 +1,13 @@
-// test.js
-const libftdi = require('./build/Release/ftdimin04');
+var libftdi = require('./build/Release/ftdimin04')
+var msg = libftdi.hello();
+
+console.log("Llamando a libftdi.hello()");
+console.log(msg)
+
+console.log("Creando contexto")
+//var ctx = libftdi.create_context();
+
+console.log("Otras pruebas")
 
 const obj = libftdi.FtdiContextWrapper(10);
 console.log(obj.plusOne());
