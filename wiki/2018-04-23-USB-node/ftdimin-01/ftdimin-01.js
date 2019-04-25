af27bc4569084376c8370b311d65df236e4cbdf0
obijuan <juan@iearobotics.com>
2019-04-25 14:09:49 +0200

    node-ftdi: ftdimin01 example updated

--- wiki/2018-04-23-USB-node/ftdimin-01/ftdimin-01.js
+++ wiki/2018-04-23-USB-node/ftdimin-01/ftdimin-01.js
@@ -1,3 +1,7 @@
-var libftdi = require('libftdi');
+var libftdi = require('./build/Release/ftdimin01')
 var msg = libftdi.hello();
+
+console.log("Llamando a libftdi.hello()");
+console.log(msg)
+
 //var ctx = libftdi.create_context();
