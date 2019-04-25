a3897e2207fa8b9a72eddf4b43c6acb072748f8c
obijuan <juan@iearobotics.com>
2019-04-25 17:09:54 +0200

    ftdimin03: example added

---
+++ wiki/2018-04-23-USB-node/ftdimin-03/addon.cc
@@ -0,0 +1,16 @@
+// addon.cc
+#include <node.h>
+#include "myobject.h"
+
+namespace demo {
+
+using v8::Local;
+using v8::Object;
+
+void InitAll(Local<Object> exports) {
+  MyObject::Init(exports);
+}
+
+NODE_MODULE(NODE_GYP_MODULE_NAME, InitAll)
+
+}  // namespace demo
---
+++ wiki/2018-04-23-USB-node/ftdimin-03/test.js
@@ -0,0 +1,10 @@
+// test.js
+const addon = require('./build/Release/addon');
+
+const obj = new addon.MyObject(10);
+console.log(obj.plusOne());
+// Prints: 11
+console.log(obj.plusOne());
+// Prints: 12
+console.log(obj.plusOne());
+// Prints: 13
