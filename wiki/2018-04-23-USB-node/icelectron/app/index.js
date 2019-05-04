6629379d922758b7691146bc3305da3a34721f07
obijuan <juan@iearobotics.com>
2019-05-04 11:57:23 +0200

    icelectron: USB detection on the main window

--- wiki/2018-04-23-USB-node/icelectron/app/index.js
+++ wiki/2018-04-23-USB-node/icelectron/app/index.js
@@ -8,12 +8,16 @@ function main()
 {
   console.log("Main loop!!")
 
+  display = document.getElementById('display')
+
   usb.on('attach', (device)=> {
     console.log("Dispositivo conectado!");
+    display.innerHTML = "OK!";
   });
 
   usb.on('detach', (devide) => {
     console.log("DESCONECTADO!")
+    display.innerHTML = "DISCONNECTED!";
   })
 
 }
