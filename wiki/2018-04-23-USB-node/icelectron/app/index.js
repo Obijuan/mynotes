6cb67d8e1256efd44c76d4bcfa268995297d2268
obijuan <juan@iearobotics.com>
2019-05-04 10:56:48 +0200

    icelectron: USB device detection, on the console

--- wiki/2018-04-23-USB-node/icelectron/app/index.js
+++ wiki/2018-04-23-USB-node/icelectron/app/index.js
@@ -7,4 +7,13 @@ console.log("Estoy en index.js")
 function main()
 {
   console.log("Main loop!!")
+
+  usb.on('attach', (device)=> {
+    console.log("Dispositivo conectado!");
+  });
+
+  usb.on('detach', (devide) => {
+    console.log("DESCONECTADO!")
+  })
+
 }
